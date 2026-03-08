3b8db62f4a3ca6d90dd3aa4030b7634a7ba6a6b5
sindbadmasry <40820886+sindbadmasry@users.noreply.github.com>
2026-03-08 15:21:07 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,8 +6,8 @@ const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
+  './icon-192192.png',
+  './icon-512x512.png',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
