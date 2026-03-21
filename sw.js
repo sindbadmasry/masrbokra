c182920281e8509ee712c7ae116e173ba2f290e0
sindbadmasry <40820886+sindbadmasry@users.noreply.github.com>
2026-03-21 08:10:14 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'masrbokra-cache-v2.6'; // قمت بزيادة الإصدار للتحديث
+const CACHE_NAME = 'masrbokra-cache-v2.7'; // قمت بزيادة الإصدار للتحديث
 
 const ASSETS_TO_CACHE = [
   './',
